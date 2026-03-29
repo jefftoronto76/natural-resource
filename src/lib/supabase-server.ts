71a552862b37628941bc25bcf96c5eb31f30b343
Jeff <56446001+jefftoronto76@users.noreply.github.com>
2026-03-29 17:24:20 -0400

    Update supabase-server.ts

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -2,8 +2,7 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
-  const cookieStore = cookies()
-
+  const cookieStore = await cookies()
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
